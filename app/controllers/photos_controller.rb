b085967f1bf70010c5aec52f3534d329cce5befd
Pedro Luis <pedro.holguin@railskarate.com>
2017-01-23 11:14:21 -0500

    Added image upload form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -71,6 +71,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:description, :user_id)
+      params.require(:photo).permit(:description, :file, :user_id)
     end
 end
