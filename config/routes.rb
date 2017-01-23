0566664423636dc6754d538aca49cc26f0fb8333
Esteban Echeverry <Esteban-0418@hotmail.com>
2017-01-23 11:28:06 -0500

    User profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+	get 'profile/show/', to: 'profile#show', as: 'show'
   resources :photos
 	root to: "home#index"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
