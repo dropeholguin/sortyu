6f1fda4963027c609ad3fcf232275df4da3040b6
Pedro Luis <pedro.holguin@railskarate.com>
2017-01-24 23:20:19 -0500

    Added scope for get photos of user

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,6 +8,8 @@ class Photo < ApplicationRecord
   has_attached_file :file, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
 
+  scope :photos, -> (user_id) { where(user_id: user_id) }
+
   aasm column: "state" do
 		state :free, initial: true
     state :paid
