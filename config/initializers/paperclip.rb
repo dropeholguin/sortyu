4229a4e6d64c90733cd1c3ae423406cb8dc2667d
Pedro Luis <pedro.holguin@railskarate.com>
2017-01-31 13:30:36 -0500

    Precompiled Assets in production environment

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
-
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip.interpolates(:s3_bucket) do |attachement, style|
   Rails.application.config.paperclip_defaults[:s3_credentials][:bucket]
 end
