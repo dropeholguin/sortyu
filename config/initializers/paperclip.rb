dffbc411951489bb199f92133696a9c6e0da1b77
Esteban Echeverry <Esteban-0418@hotmail.com>
2017-02-06 17:42:30 -0500

    uncommented paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
-# # Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-# # Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-# Paperclip.interpolates(:s3_bucket) do |attachement, style|
-#   Rails.application.config.paperclip_defaults[:s3_credentials][:bucket]
-# end
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip.interpolates(:s3_bucket) do |attachement, style|
+  Rails.application.config.paperclip_defaults[:s3_credentials][:bucket]
+end
 Paperclip::Attachment.default_options[:s3_protocol] = "https"
