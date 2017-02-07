9df70f003d63918905e89caeee6d9c5df38a842e
Sebastian Velandia <sebas.velandia@grupo.ly>
2017-02-07 18:10:59 -0500

    Hacked Trackingjs, added  after

--- vendor/assets/bower_components/tracking.js/src/utils/Canvas.js
+++ vendor/assets/bower_components/tracking.js/src/utils/Canvas.js
@@ -21,7 +21,6 @@
   tracking.Canvas.loadImage = function(canvas, src, x, y, width, height, opt_callback) {
     var instance = this;
     var img = new window.Image();
-    img.crossOrigin = '*';
     img.onload = function() {
       var context = canvas.getContext('2d');
       canvas.width = width;
@@ -32,6 +31,7 @@
       }
       img = null;
     };
+    img.crossOrigin = '*';
     img.src = src;
   };
 }());
