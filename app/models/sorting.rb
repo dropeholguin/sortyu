a65e13bec991c564e70702b22f6387677d99080c
Esteban Echeverry <Esteban-0418@hotmail.com>
2017-02-22 17:06:47 -0500

    Sections scope created

--- app/models/sorting.rb
+++ app/models/sorting.rb
@@ -1,4 +1,5 @@
 class Sorting < ApplicationRecord
-  belongs_to :user
-  belongs_to :section
+	scope :user_sorting, ->(user_id, photo_id) { where(user_id: user_id).joins(:section).where("sections.photo_id = ?", photo_id) }
+	belongs_to :user
+	belongs_to :section
 end
