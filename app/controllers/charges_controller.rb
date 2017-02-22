c3fb707ae01f6784853f6c41883cafebf5fd62fa
Pedro Luis <pedro.holguin@railskarate.com>
2017-02-22 08:53:33 -0500

    Fixed pay per images

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,21 +8,23 @@ class ChargesController < ApplicationController
 	def create
 	  # Amount in cents
 	  @amount = 500
+	  @photo = Photo.find(params[:photo_id])
 
 	  customer = Stripe::Customer.create(
 	    email: 	params[:stripeEmail],
 	    source: params[:stripeToken]
 	  )
-
+	  
 	  charge = Stripe::Charge.create(
 	    customer:    	customer.id,
 	    amount:       @amount,
 	    description: 	'Rails Stripe customer',
 	    currency: 		'usd'
 	  )
+	  @photo.pay!
+		rescue Stripe::CardError => e
+		  flash[:error] = e.message
+		  redirect_to new_charge_path
 		
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to new_charge_path
 	end
 end
