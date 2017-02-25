8496cd16f34c008386a85acc2a18ffbfcadddb79
Sebastian Velandia <sebas.velandia@grupo.ly>
2017-02-25 18:25:35 -0500

    Now stats are identified with the order user clicked sections

--- app/models/sorting.rb
+++ app/models/sorting.rb
@@ -1,4 +1,6 @@
 class Sorting < ApplicationRecord
   belongs_to :user
   belongs_to :section
+
+  scope :user_sortings, ->(user_id, photo_id) { where(user_id: user_id).joins(:section).where("sections.photo_id = ?", photo_id) }
 end
