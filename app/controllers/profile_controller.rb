c96d3f920ffd84b44c9fc8fa5191d285e8f9c42c
Pedro Luis <pedro.holguin@railskarate.com>
2017-03-17 12:35:57 -0500

    Added filters to profile and removed filters in sorting page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,17 @@ class ProfileController < ApplicationController
 	before_filter :authenticate_user!
 	
 	def show
-		@photos = Photo.photos(current_user).order('created_at desc')
+		@photos = []
+		case params[:sort_by].to_i
+        when 1
+            @photos = Photo.photos(current_user).order( 'count_of_sorts DESC' )
+        when 2
+            @photos = Photo.photos(current_user).order(cached_votes_up: :desc)
+        when 3
+            @photos = Photo.photos(current_user).order( 'state DESC' )
+        else
+			@photos = Photo.photos(current_user).order('created_at desc')        
+		end
 	end
 
 	def other_user_show
