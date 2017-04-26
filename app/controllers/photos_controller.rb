7a61add8a7c6db5a323d39a45ba34765b1a88f1d
Esteban Echeverry <Esteban-0418@hotmail.com>
2017-04-26 16:20:24 -0500

    created recent sort path

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -389,6 +389,10 @@ class PhotosController < ApplicationController
         end
     end
 
+    def recent_sorts
+        
+    end
+
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
