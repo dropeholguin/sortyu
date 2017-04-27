b2dec59e42c7007107755c104e24c038f9fc3831
Pedro Luis <dropeholguin1994@gmail.com>
2017-04-27 09:44:15 -0500

    Fixed Price format when paying for images

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 	def new
 		if params[:photo_id].present?
 			@photo = Photo.find(params[:photo_id])
-			@price = Price.last.value_cents
+			@price = Price.last.value
 		elsif cookies[:pay_photos].present?
 			@photos = []
 			photo_ids_array = cookies[:pay_photos].split("-")
@@ -12,7 +12,7 @@ class ChargesController < ApplicationController
 				@photos << Photo.find(id_photo.to_i)
 			end
 			@count = @photos.count
-			@price = Price.last.value_cents * @count
+			@price = Price.last.value * @count
 		end
 	end
 
@@ -20,7 +20,7 @@ class ChargesController < ApplicationController
 		@user = current_user
 		# Amount in cents
 		if params[:photo_id].present? && Photo.find(params[:photo_id]).state == "free"
-			@amount =  Price.last.value_cents
+			@amount =  Price.last.value
 			customer = Stripe::Customer.create(
 				email: 	params[:stripeEmail],
 				source: params[:stripeToken]
@@ -28,7 +28,7 @@ class ChargesController < ApplicationController
 
 			charge = Stripe::Charge.create(
 				customer:    	customer.id,
-				amount:       @amount,
+				amount:       Price.last.value_cents,
 				description: 	'Rails Stripe customer',
 				currency: 		'usd'
 			)
@@ -43,7 +43,7 @@ class ChargesController < ApplicationController
 			end
 		elsif cookies[:pay_photos].present?
 			@count = cookies[:pay_photos].split("-").count
-			@amount =  Price.last.value_cents * @count
+			@amount =  Price.last.value * @count
 			
 			customer = Stripe::Customer.create(
 				email: 	params[:stripeEmail],
@@ -52,7 +52,7 @@ class ChargesController < ApplicationController
 
 			charge = Stripe::Charge.create(
 				customer:    	customer.id,
-				amount:       @amount,
+				amount:       Price.last.value_cents,
 				description: 	'Rails Stripe customer',
 				currency: 		'usd'
 			)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425215832) do
+ActiveRecord::Schema.define(version: 20170427141815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20170425215832) do
     t.boolean  "is_active",              default: false
     t.text     "about_you"
     t.integer  "balance_cents",          default: 0
+    t.string   "ref"
     t.index ["email"], name: "index_affiliates_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_affiliates_on_reset_password_token", unique: true, using: :btree
   end
@@ -154,6 +155,7 @@ ActiveRecord::Schema.define(version: 20170425215832) do
     t.integer  "value_cents"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "currency"
   end
 
   create_table "relationships", force: :cascade do |t|
