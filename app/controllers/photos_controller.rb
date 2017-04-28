715c0a81d9c94cfc02b28e0d27640bae786f1117
Esteban Echeverry <Esteban-0418@hotmail.com>
2017-04-28 17:59:56 -0500

    Users can check their last sort

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -390,7 +390,7 @@ class PhotosController < ApplicationController
     end
 
     def recent_sorts
-        
+        @photos = Photo.photos(current_user).order('created_at desc') 
     end
 
     private
