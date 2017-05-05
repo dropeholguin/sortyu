8310b845d6aff11129847ea37484e1967d984b5a
Pedro Luis <dropeholguin1994@gmail.com>
2017-05-04 22:10:26 -0500

    Added before_filter authenticate user to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
     before_action :set_photo, only: [:show, :edit, :update, :destroy, :shared_times, :like, :unlike]
+    before_filter :authenticate_user!
     # GET /photos
     # GET /photos.json
     def index
